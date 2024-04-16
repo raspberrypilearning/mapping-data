7baa00aae4d1a1e99c2218e765aa4727ccf0c7a9
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-04-16 16:53:09 +0100

    New translations main.py (French)

--- fr-FR/code/mapping_data_population/main.py
+++ fr-FR/code/mapping_data_population/main.py
@@ -3,69 +3,69 @@ from p5 import *
 from regions import get_region_coords
 from random import randint
 
-region_list = []
-colours = {}
+liste_regions = []
+couleurs = {}
 
 def preload():
-    global map
-    map = load_image('mercator_bw.png')
+    global carte
+    carte = load_image('mercator_bw.png')
 
 
-# Put code to run once here
+# Mets le code à exécuter une seule fois ici
 def setup():
     size(991, 768)
-    load_data('pop.csv')
+    charge_donnees('pop.csv')
     image(
-        map,  # The image to draw
-        0,  # The x of the top-left corner
-        0,  # The y of the top-left corner
-        width,  # The width of the image
-        height  # The height of the image
+        carte,  # L'image à dessiner
+        0,  # Le x du coin supérieur gauche
+        0,  # Le y du coin supérieur gauche
+        width,  # La largeur de l'image
+        height  # La hauteur de l'image
     )
-    draw_pin(300, 300, Color(255, 0, 0))
-    draw_data()
+    dessine_epingle(300, 300, Color(255, 0, 0))
+    dessine_donnees()
 
 
-def draw_pin(x_coord, y_coord, colour):
+def dessine_epingle(x_coord, y_coord, couleur):
     no_stroke()
-    fill(colour)
+    fill(couleur)
     rect(x_coord, y_coord, 10, 10)
 
-def load_data(file_name):
-    with open(file_name) as f:
-        for line in f:
-            info = line.split(',')
-            region_dict = {
+def charge_donnees(nom_fichier):
+    with open(nom_fichier) as f:
+        for ligne in f:
+            info = ligne.split(',')
+            dico_regions = {
                 'region': info[0],
                 'population': info[1],
-                'population density': info[2]
+                'densite population': info[2]
             }
-            region_list.append(region_dict)
+            liste_regions.append(dico_regions)
 
-def draw_data():
-    green_value = 255
-    for region in region_list:
-        region_name = region['region']  # Get the name of the region
-        region_coords = get_region_coords(region_name)  # Use the name to get coordinates
-        region_x = region_coords['x']  # Get the x coordinate
-        region_y = region_coords['y']  # Get the y coordinate
-        #print(region_name, region_x, region_y)
-        region_colour = Color(0, green_value, 0)  # Set the pin colour
-        colours[region_colour.hex] = region
-        draw_pin(region_x, region_y, region_colour)
-        green_value -= 1
+def dessine_donnees():
+    valeur_vert = 255
+    for region in liste_regions:
+        nom_region = region['region']  # Récupère le nom de la région
+        coords_region = get_region_coords(nom_region)  # Utilise le nom pour obtenir les coordonnées
+        region_x = coords_region['x']  # Récupère la coordonnée x
+        region_y = coords_region['y']  # Récupère la coordonnée y
+        #print(nom_region, region_x, region_y)
+        couleur_region = Color(0, valeur_vert, 0)  # Définis la couleur de l'épingle
+        couleurs[couleur_region.hex] = region
+        dessine_epingle(region_x, region_y, couleur_region)
+        valeur_vert -= 1
     
-# Put code to run when the mouse is pressed here
+# Place ici le code à exécuter lorsque la souris est cliquée
 def mouse_pressed():
-    pixel_colour = Color(get(mouse_x, mouse_y)).hex
+    couleur_pixel = Color(get(mouse_x, mouse_y)).hex
     
-    if pixel_colour in colours:
-        facts = colours[pixel_colour]
-        print('Name: ', facts['region'])
-        print('Population: ', facts['population'])
-        print('Population density', facts['population density'])
+    if couleur_pixel in couleurs:
+        faits = couleurs[couleur_pixel]
+        print('Nom: ', faits['region'])
+        print('Population: ', faits['population'])
+        print('Densité de population', faits['densite population'])
     else:
-        print('Region not detected')
+        print('Région non détectée')
 
 
 run()
