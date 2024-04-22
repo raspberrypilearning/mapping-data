0c04b5faf955f4043239398f58ca329961a5e7c0
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-04-22 16:20:20 +0100

    New translations main.py (Dutch)

--- nl-NL/code/mapping_data_happiness_ink_map/main.py
+++ nl-NL/code/mapping_data_happiness_ink_map/main.py
@@ -1,73 +1,73 @@
 #!/bin/python3
 from p5 import *
-from regions import get_region_coords
+from regions import haal_regio_coördinaten
 from random import randint, seed
 
-region_list = []
-colours = {}
+regio_lijst = []
+kleuren = {}
 
 
 def preload():
-    global map
-    map = load_image('ink_map.jpg')
+    wereldkaart
+    kaart = load_image('ink_map.jpg')
 
 
 def setup():
-    # Put code to run once here
+    # Zet de code om eenmalig uit te voeren hier onder
     size(991, 768)
-    load_data('happy.csv')
+    load_data ('happy.csv')
     image(
-        map,  # The image to draw
-        0,  # The x of the top-left corner
-        0,  # The y of the top-left corner
-        width,  # The width of the image
-        height  # The height of the image
+        kaart, # De afbeelding om te tekenen
+        0, # De x van de linkerbovenhoek
+        0, # De y van de linker bovenhoek
+        width, # De breedte van de afbeelding
+        height # De hoogte van de afbeelding
     )
-    draw_data()
+    teken_gegevens()
 
 
-def mouse_pressed():
-    # Put code to run when the mouse is pressed here
-    pixel_colour = Color(get(mouse_x, mouse_y)).hex
+def muis_ingedrukt():
+    # Zet code hier die moet worden uitgevoerd wanneer de muis wordt ingedrukt
+    pixel_kleur = Color(get(mouse_x, mouse_y)).hex
     try:
-        facts = colours[pixel_colour]
-        print(facts['name'])
-        print(facts['happiness rank'])
+        feiten = kleuren[pixel_kleur]
+        print(feiten['naam'])
+        print(feiten['ranking geluk'])
     except KeyError:
-        print('Click on a pin')
+        print('Klik op een speld')
 
 
-def load_data(file_name):
+def laad_gegevens(file_name):
     with open(file_name) as f:
         for line in f:
             # print(line)
             info = line.split(',')
-            region_dict = {
-                'name': info[0],
-                'happiness rank': int(info[1]),
-                'happiness score': float(info[2])
+            regio_dict = {
+                'naam': info[0],
+                'ranking geluk': int(info[1]),
+                'geluksscore': float(info[2])
             }
-            # print(region_dict)
-            region_list.append(region_dict)
+            # print(regio_dict)
+            regio_lijst.append(regio_dict)
 
 
-def draw_pin(x, y, colour):
-    fill(colour)
+def teken_speld(x, y, kleur):
+    fill(kleur)
     ellipse(x, y, 10, 10)
 
 
-def draw_data():
+def teken_gegevens():
     seed(10)
-    for region in region_list:
-        region_name = region['name']  # Get the name of the region
-        # Use the name to get coordinates
-        region_coords = get_region_coords(region_name)
-        region_x = region_coords['x']  # Get the x coordinate
-        region_y = region_coords['y']  # Get the y coordinate
-        region_colour = Color(randint(0, 255), randint(
-            0, 255), randint(0, 255))  # Set the pin colour
-        colours[region_colour.hex] = region
-        draw_pin(region_x, region_y, region_colour)
+    for regio in regio_lijst:
+        regio_naam = regio['naam'] # Haal de naam op van de regio
+        # Gebruik de naam om coördinaten te krijgen
+        regio_coördinaten = haal_regio_coördinaten(regio_naam)
+        regio_x = regio_coördinaten['x'] # Haal de x-coördinaat op
+        regio_y = regio_coördinaten['y'] # Haal de y-coördinaat op
+        region_color = Color(randint(0, 255), randint(
+            0, 255), randint(0, 255)) # Stel de speldkleur in
+        kleuren[regio_kleur.hex] = regio
+        teken_speld(regio_x, regio_y, regio_kleur)
 
 
 run()
