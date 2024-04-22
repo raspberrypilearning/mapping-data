f3c6f03c24ee47178aed0535cdb12d900268e416
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-04-22 16:20:27 +0100

    New translations main.py (Dutch)

--- nl-NL/code/mapping_data_starter/main.py
+++ nl-NL/code/mapping_data_starter/main.py
@@ -1,17 +1,17 @@
 #!/bin/python3
 from p5 import *
-from regions import get_region_coords
+from regions import haal_regio_coördinaten
 
-# Put code to run once here
+# Zet de code om eenmalig uit te voeren hier onder
 
 
 def setup():
     pass
 
 
-# Put code to run when the mouse is pressed here
-def mouse_pressed():
-    pixel_colour = Color(get(mouse_x, mouse_y)).hex
+# Zet code hier die moet worden uitgevoerd wanneer de muis wordt ingedrukt
+def muis_ingedrukt():
+    pixel_kleur = Color(get(mouse_x, mouse_y)).hex
 
 
 run()
